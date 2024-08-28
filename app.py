b15fff8cc9a2beb8e4b2099ea0344e9e2bca6a5a
Jenny Medina <medina.stsci@gmail.com>
2024-08-28 13:48:05 -0400

    A simplified Streamlit app

--- app.py
+++ app.py
@@ -1,72 +1,56 @@
 import numpy as np
 import streamlit as st
-from toolkit.queries import (
-    query_entity_distribution,
-    query_project_downloads,
-    query_project_sizes,
-    query_unique_users,
-)
+from toolkit.queries import query_users_trend
 from toolkit.utils import get_data_from_snowflake
-from toolkit.widgets import plot_download_sizes, plot_unique_users_trend
+from toolkit.widgets import ( scatter_plot_yearly_unique_users, 
+    bar_plot_yearly_unique_users)
 
 # Configure the layout of the Streamlit app page
 st.set_page_config(layout="wide",
-                   page_title="HTAN Analytics",
+                   page_title="My Streamlit App",
                    page_icon=":bar_chart:",
                    initial_sidebar_state="expanded")
 
-# Custom CSS for styling
+# Custom CSS for styling (Optional)
 with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+# Add user interface elements to sidebar
+plot_options = ["Dataframe", "Line plot", "Bar plot"]
+selected_plot = st.sidebar.selectbox("Choose a plot type", plot_options)
 
-def main():
+def app(selected_plot):
 
     # 1. Retrieve the data using your queries in queries.py
-    entity_distribution_df = get_data_from_snowflake(query_entity_distribution())
-    project_sizes_df = get_data_from_snowflake(query_project_sizes())
-    project_downloads_df = get_data_from_snowflake(query_project_downloads())
-    # User input for the number of months
-    months_back = st.sidebar.slider("Lookback Range (how many months back to display trends)",
-                                    min_value=1,
-                                    max_value=24,
-                                    value=12)
-    # Use the selected months_back in the unique users query
-    unique_users_df = get_data_from_snowflake(query_unique_users(months_back))
-
-    # 2. Transform the data as needed
-    convert_to_gib = 1024 * 1024 * 1024
-    project_sizes = dict(
-        PROJECT_ID=list(project_sizes_df["PROJECT_ID"]),
-        TOTAL_CONTENT_SIZE=list(project_sizes_df["TOTAL_CONTENT_SIZE"]),
-    )
-    total_data_size = sum(
-        project_sizes["TOTAL_CONTENT_SIZE"]
-    )  # round(sum(project_sizes['TOTAL_CONTENT_SIZE']) / convert_to_gib, 2)
-    average_project_size = round(
-        np.mean(project_sizes["TOTAL_CONTENT_SIZE"]) / convert_to_gib, 2
-    )
-
-    # 3. Format the app, and visualize the data with your widgets in widgets.py
     # -------------------------------------------------------------------------
-    # Row 1 -------------------------------------------------------------------
-    st.markdown("### Monthly Overview :calendar:")
-    col1, col2, col3 = st.columns([1, 1, 5])
-    col1.metric("Total Storage Occupied", f"{total_data_size} GB", "7.2 GB")
-    col2.metric("Avg. Project Size", f"{average_project_size} GB", "8.0 GB")
-    col3.metric("Annual Cost", "102,000 USD", "10,000 USD")
+    users_trend_df = get_data_from_snowflake(query_users_trend())
 
-    # Row 2 -----------------------------------------------------------------
-    st.markdown("### Unique Users Report :bar_chart:")
-    st.plotly_chart(plot_unique_users_trend(unique_users_df))
+    # 2. Display the data using widgets in widgets.py
+    # -------------------------------------------------------------------------
 
-    # Row 3 -------------------------------------------------------------------
-    st.plotly_chart(plot_download_sizes(project_downloads_df, project_sizes_df))
+    if selected_plot == "Dataframe":
+        st.write("### User Growth Dataframe\nHere we display the cumulative growth of unique users over the years in a straight-forward dataframe. Each row represents the total number of users up to and including that year, allowing you to see how user account creation has increased over time.")
+        st.dataframe(users_trend_df,
+                     hide_index=True,
+                     width=600,
+                     column_config={
+                        "QUERY_YEAR": st.column_config.TextColumn(
+                            "Year",
+                        ),
+                        "CUMULATIVE_USERS": st.column_config.TextColumn(
+                            "Total Users"
+                        )
+                        }
+                    )
 
-    # Row 4 -------------------------------------------------------------------
-    st.markdown("### Entity Trends :pencil:")
-    st.dataframe(entity_distribution_df)
+    if selected_plot == "Line plot":
+        st.write("### Yearly User Growth Trend\nThis line plot illustrates the growth trend of unique users over the years. The dots on the line represent the total number of users up to the end of each year, providing a clear view of how user growth has progressed over time.")
+        st.plotly_chart(scatter_plot_yearly_unique_users(users_trend_df))
 
+    if selected_plot == "Bar plot":
+        st.write("### Yearly User Growth with Percentage of Total\nThis bar plot shows the cumulative number of unique users up to the end of each year. The overlaid line indicates the percentage of total users achieved by that year, giving you a sense of growth relative to the overall total.")
+        st.plotly_chart(bar_plot_yearly_unique_users(users_trend_df))
+        
 
 if __name__ == "__main__":
-    main()
+    app(selected_plot)
